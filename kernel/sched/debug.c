add3672473515bca086caf65cae3e1c9e2e411a5
Todd Kjos <tkjos@google.com>
2015-10-14 16:41:34 -0700

    Revert "[ALPS01783144] sched: merge EAS partial patch sets"
    This reverts commit aa25c55a9ec10b2ad6474aecd9ce6de04c35603c.

--- kernel/sched/debug.c
+++ kernel/sched/debug.c
@@ -101,7 +101,7 @@ static void print_cfs_group_stats(struct seq_file *m, int cpu, struct task_group
 	if (!se) {
 		struct sched_avg *avg = &cpu_rq(cpu)->avg;
 		P(avg->runnable_avg_sum);
-		P(avg->avg_period);
+		P(avg->runnable_avg_period);
 #ifdef MTK_SCHED_CMP_PRINT
 		P(avg->last_runnable_update);
 #endif
@@ -127,10 +127,8 @@ static void print_cfs_group_stats(struct seq_file *m, int cpu, struct task_group
 	P(se->load.weight);
 #ifdef CONFIG_SMP
 	P(se->avg.runnable_avg_sum);
-	P(se->avg.running_avg_sum);
-	P(se->avg.avg_period);
+	P(se->avg.runnable_avg_period);
 	P(se->avg.load_avg_contrib);
-	P(se->avg.utilization_avg_contrib);
 	P(se->avg.decay_count);
 #endif
 #undef PN
@@ -307,8 +305,6 @@ void print_cfs_rq(struct seq_file *m, int cpu, struct cfs_rq *cfs_rq)
 			cfs_rq->runnable_load_avg);
 	SEQ_printf(m, "  .%-30s: %ld\n", "blocked_load_avg",
 			cfs_rq->blocked_load_avg);
-	SEQ_printf(m, "  .%-30s: %ld\n", "utilization_load_avg",
-			cfs_rq->utilization_load_avg);
 #ifdef CONFIG_FAIR_GROUP_SCHED
 	SEQ_printf(m, "  .%-30s: %ld\n", "tg_load_contrib",
 			cfs_rq->tg_load_contrib);
@@ -747,10 +743,8 @@ void proc_sched_show_task(struct task_struct *p, struct seq_file *m)
 	P(se.load.weight);
 #ifdef CONFIG_SMP
 	P(se.avg.runnable_avg_sum);
-	P(se.avg.running_avg_sum);
-	P(se.avg.avg_period);
+	P(se.avg.runnable_avg_period);
 	P(se.avg.load_avg_contrib);
-	P(se.avg.utilization_avg_contrib);
 	P(se.avg.decay_count);
 #endif
 	P(policy);
@@ -962,7 +956,7 @@ static void print_cfs_group_stats_at_AEE(struct seq_file *m, int cpu, struct tas
 		struct sched_avg *avg = &cpu_rq(cpu)->avg;
 
 		P(avg->runnable_avg_sum);
-		P(avg->avg_period);
+		P(avg->runnable_avg_period);
 #ifdef MTK_SCHED_CMP_PRINT
 		P(avg->last_runnable_update);
 #endif
@@ -988,7 +982,7 @@ static void print_cfs_group_stats_at_AEE(struct seq_file *m, int cpu, struct tas
 	P(se->load.weight);
 #ifdef CONFIG_SMP
 	P(se->avg.runnable_avg_sum);
-	P(se->avg.avg_period);
+	P(se->avg.runnable_avg_period);
 	P(se->avg.load_avg_contrib);
 	P(se->avg.decay_count);
 # ifdef MTK_SCHED_CMP_PRINT
